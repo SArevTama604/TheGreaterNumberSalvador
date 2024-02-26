458eabfec32539a02493fb5dd36f66e1c8c51d62
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-25 18:42:36 -0800

    Update Program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("The first number is greater than the second number.");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("the second number is greater than the first number."); ;
+            }
+            else
+            {
+                Console.WriteLine("Both number are equal.");
+            }
         }
     }
 }
